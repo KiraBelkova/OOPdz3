952cd9baeac4cfeb7ef04545ee2fc9fc09f1bc26
Кира Белькова <kirabelkova2013@yandex.ru>
2024-03-20 11:03:03 +0300

    добавила класс Group

---
+++ Group.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Group {
+    private String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
